524d2a4c3f88f432b7bb7810386738505edf0797
rgnrlthbrk <devastatorBro1>
2017-09-21 17:39:55 +0300

    logger

--- src/test/java/AuthorTest.java
+++ src/test/java/AuthorTest.java
@@ -2,17 +2,20 @@ import jpa.WebApplication;
 import jpa.domain.Author;
 import jpa.domain.Book;
 import jpa.repositories.AuthorRepository;
-import jpa.repositories.BookRepository;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+
 
 import static org.hamcrest.Matchers.*;
 
@@ -24,7 +27,7 @@ import static org.hamcrest.Matchers.*;
 @Transactional
 public class AuthorTest
 {
-
+  private final Logger logger = LoggerFactory.getLogger(AuthorTest.class);
   @Autowired
   AuthorRepository authorRepository;
 
